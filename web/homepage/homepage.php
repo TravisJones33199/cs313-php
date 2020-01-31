48d69cc6dea8d0f2bae5535702e7bd4ebe920b26
Travis Jones <tjones33199@gmail.com>
2020-01-31 10:28:39 -0800

    homepage

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -11,35 +11,7 @@
     <div class="main-header">
         <h1 class="main-header">Travis Jones' Homepage</h1>
         <?php
-            var xmlHttp;
-            function srvTime(){
-                try {
-                    //FF, Opera, Safari, Chrome
-                    xmlHttp = new XMLHttpRequest();
-                }
-                catch (err1) {
-                    //IE
-                    try {
-                        xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
-                    }
-                    catch (err2) {
-                        try {
-                            xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-                        }
-                        catch (eerr3) {
-                            //AJAX not supported, use CPU time.
-                            alert("AJAX not supported");
-                        }
-                    }
-                }
-                xmlHttp.open('HEAD',window.location.href.toString(),false);
-                xmlHttp.setRequestHeader("Content-Type", "text/html");
-                xmlHttp.send('');
-                return xmlHttp.getResponseHeader("Date");
-            }
-            
-            var st = srvTime();
-            var date = new Date(st);
+            echo getdate();
         ?>
     </div>
     <ul>
