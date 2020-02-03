45e65934259f6c2bb4d12a2c23a001bcc8697b56
Travis Jones <tjones33199@gmail.com>
2020-02-03 11:34:57 -0800

    Update week3

--- web/Week 3/shop.php
+++ web/Week 3/shop.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +16,16 @@
 </head>
 <body>
     <h1>Shop</h1>
+
+    <form method="post">
+        <button type="submit" name="item[]" value="Item 1">Item 1</button>
+        <button type="submit" name="item[]" value="Item 2">Item 2</button>
+    </form>
+
+    <?
+    array_push($_SESSION['cart'], $_POST['item'])
+
+    print_r($_SESSION['cart']);
+    ?>
 </body>
 </html>
